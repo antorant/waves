ab47d68ecf47eea6f03a8fa3a74d6bdeff5b36dc
antorant <antonyorant@gmail.com>
2016-02-07 22:21:45 +0100

    Add vibrations

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ renderWaveform( $('.waveform'), 100, 4 )
 $('body').keyup(function(e){
   console.log(e.keyCode)
   if(e.keyCode == 32 || e.keyCode == 13){ // space bar / return
-     renderWaveform( $('.waveform'), 100, 4 )
+     renderWaveform( $('.waveform'), 200, 4 )
   }
 })
